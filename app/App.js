a4887a8147395bd2c52fcc1a0293027bd69d2421
Arthur Simiyu <hello@arthursimiyu.com>
2021-07-27 19:09:35 +0300

    Add Icons on Navigation Tabs

--- app/App.js
+++ app/App.js
@@ -30,7 +30,24 @@ const Tab = createBottomTabNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = focused ? 'home' : 'home';
+            } else if (route.name === 'Explore') {
+              iconName = focused ? 'compass' : 'compass';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'person' : 'person';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'blue',
+          inactiveTintColor: 'gray',
+        }}>
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Explore" component={Explore} />
         <Tab.Screen name="Profile" component={Profile} />
