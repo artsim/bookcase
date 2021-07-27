bac6a44554ef66021918dffe27feb5fa486f577b
Arthur Simiyu <hello@arthursimiyu.com>
2021-07-28 01:18:43 +0300

    Fix class name BookCase spelling

--- app/screens/BookCase.js
+++ app/screens/BookCase.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
-export default class Boookcase extends Component {
+export default class BookCase extends Component {
   render() {
     return (
       <View style={styles.container}>
